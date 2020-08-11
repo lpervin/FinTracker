523461a5f3d9a7bc29e7287904ffbf2df12c959d
lenny <lpervin@gmail.com>
2020-08-10 21:46:22 -0400

    Added drawer menu

--- FinTracker.UI/ClientApp/src/components/Layout.js
+++ FinTracker.UI/ClientApp/src/components/Layout.js
@@ -1,24 +1,53 @@
 import React, {Component} from 'react'
+import clsx from "clsx";
+import makeStyles  from '@material-ui/core/styles/makeStyles';
 import NavMenu from "./NavMenu";
 import Container from '@material-ui/core/Container'
-import './NavMenu.css'
 
-
-class Layout extends Component {
-  
-    render() {
-        const styles = {root: { paddingTop: 150 }}
-        return (
-            <React.Fragment>
-                <NavMenu/>                
-                <Container className="page-content">
-                    {this.props.children}   
-                </Container>                
-            </React.Fragment>
+const drawerWidth = 240;
+const useStyles = makeStyles((theme) => ({
+        root: {
+            display: 'flex',
+        },
+            content: {
+                flexGrow: 1,
+                padding: theme.spacing(3),
+                transition: theme.transitions.create('margin', {
+                    easing: theme.transitions.easing.sharp,
+                    duration: theme.transitions.duration.leavingScreen,
+                }),
                 
-            
-        );
-    }
-}
+            },
+            contentShift: {
+                transition: theme.transitions.create('margin', {
+                    easing: theme.transitions.easing.easeOut,
+                    duration: theme.transitions.duration.enteringScreen,
+                }),
+                marginLeft: drawerWidth,
+            },
+    }));
 
-export default  Layout;
+    export default  function Layout(props)  {
+        const classes = useStyles();
+        const [open, setOpen] = React.useState(false);
+       const handleMenuOpen = (isOpen) => {
+            setOpen(isOpen)
+            //console.log(isOpen)           
+        };
+        
+            return  (
+                <React.Fragment>
+                    <NavMenu toggleMenu={handleMenuOpen}/>
+                    <Container
+                        className={clsx(classes.content, {
+                            [classes.contentShift]: open,
+                        })}
+                    >
+                        {props.children}
+                    </Container>
+                </React.Fragment>            
+            );
+        
+    }
+
+
--- FinTracker.UI/ClientApp/src/components/NavMenu.js
+++ FinTracker.UI/ClientApp/src/components/NavMenu.js
@@ -1,31 +1,76 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import clsx from "clsx";
 import { withRouter } from 'react-router-dom'
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import MenuIcon from '@material-ui/icons/Menu';
-import makeStyles from "@material-ui/core/styles/makeStyles";
+
+
 import { Link as MaterialLink } from "@material-ui/core";
-//import Link from "@material-ui/core/Link";
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+
 import './NavMenu.css'
 
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import List from "@material-ui/core/List";
+import Drawer from "@material-ui/core/Drawer";
 
 
-const useStyles = makeStyles((theme) => ({
-    appBar: {
-        position: "relative"
-    },
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({    
   root: {
-    flexGrow: 1,
+    display: 'flex'
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
+    appBar: {
+        position: "relative",
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+      menuButton: {
+        marginRight: theme.spacing(2),
+      },
+        hide: {
+            display: 'none',
+        },
+        drawer: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+        drawerPaper: {
+            width: drawerWidth,
+        },
+        drawerHeader: {
+            display: 'flex',
+            alignItems: 'center',
+            padding: theme.spacing(0, 1),
+            // necessary for content to be below app bar
+            ...theme.mixins.toolbar,
+            justifyContent: 'flex-end',
+        },
   nav: {
     flexGrow: 1,
       baseline: '1',
@@ -50,50 +95,87 @@ const useStyles = makeStyles((theme) => ({
 
  function NavMenu(props) {
      const classes = useStyles();
-
-     const loc = props.location.pathname;
-     const links = ['/portfolio', '/research', '/trade'];
-     //console.log(loc)
-     const index = links.indexOf(loc) === -1 ? 0 : links.indexOf(loc);
-     const  [tabIndex, setValue] = React.useState(index);
+     const theme = useTheme();
+         /**Main Navigation Links **/
+             const loc = props.location.pathname;
+             const links = ['/portfolio', '/research', '/trade'];
+             //console.log(loc)
+             const index = links.indexOf(loc) === -1 ? 0 : links.indexOf(loc);
+             const  [tabIndex, setValue] = React.useState(index);
     
-     const tabChanged = (event,newValue) => {
-         console.log(newValue)
-         setValue(newValue)
-     };
+             const tabChanged = (event,newValue) => {
+                 console.log(newValue)
+                 setValue(newValue)
+             };
+        /*******/
      
-    // const classes = this.useStyles();
-
+        /****Drawer******/
+        const [isOpen, setOpen] = React.useState(false)
+        const toggleDrawer = () => {
+            const menuState = !isOpen;
+            setOpen(menuState);
+            props.toggleMenu(menuState);
+        };
+        /**********/
+     
+    
     return (
-        <AppBar  position="absolute" className={classes.appBar}>
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-              <Tabs value={tabIndex} onChange={tabChanged} aria-label="Tabs">
-                  <Tab label="Portfolio" component={MaterialLink} href="/portfolio" id="item-0" color="inherit"  classes={{ root: classes.tabItem }}/>
-                  <Tab label="Research" component={MaterialLink} href="/research" id="item-2" color="inherit"  classes={{ root: classes.tabItem }}/>
-                  <Tab label="Trade" component={MaterialLink}  href="/trade"  id="item-3"  color="inherit"  classes={{ root: classes.tabItem }}/>
-              </Tabs>
-              <Typography variant="h6">
-                  {tabIndex}
-              </Typography>
-          {/*  <Typography variant="h6" className={classes.nav}>
-                <Link href="/portfolio"  color="inherit" className={classes.navlink}>
-                   Portfolio
-                </Link>
-                
-                <Link href="/research" color="inherit" className={classes.navlink}>
-                    Research
-                </Link>
-                { ' ' }
-                <Link href="/trade" color="inherit" className={classes.navlink}>
-                    Trade
-                </Link>
-            </Typography>*/}
-            {/*<Button color="inherit">Login</Button>*/}
-          </Toolbar>
-        </AppBar>
+        <div className={classes.root}>
+            <AppBar  position="fixed" className={clsx(classes.appBar, {
+                [classes.appBarShift]: isOpen,
+            })}>
+                <Toolbar>
+                    <IconButton color="inherit" edge="start" aria-label="open drawer" 
+                                className={clsx(classes.menuButton, isOpen && classes.hide)}
+                                onClick={toggleDrawer}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Tabs value={tabIndex} onChange={tabChanged} aria-label="Tabs">
+                        <Tab label="Portfolio" component={MaterialLink} href="/portfolio" id="item-0" color="inherit"  classes={{ root: classes.tabItem }}/>
+                        <Tab label="Research" component={MaterialLink} href="/research" id="item-2" color="inherit"  classes={{ root: classes.tabItem }}/>
+                        <Tab label="Trade" component={MaterialLink}  href="/trade"  id="item-3"  color="inherit"  classes={{ root: classes.tabItem }}/>
+                    </Tabs>
+                    <Typography variant="h6">
+                        {tabIndex}
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={isOpen}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={toggleDrawer}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </div>
+                <Divider />
+                <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider />
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+        </div>
+      
         
     );  
 }
